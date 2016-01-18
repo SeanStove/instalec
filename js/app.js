a7d1398788a4329d3a4830dcf7b0e879912da99d
Sean Stove <sean.stove@gmail.com>
2016-01-18 17:20:49 +0100

    Added carousel on the homepage

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').owlCarousel(
+    {
+      navigation: false,
+      pagination: false,
+      autoPlay: 7000,
+      paginationSpeed: 1200,
+      singleItem: true
+    });
+});
